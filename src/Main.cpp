4b4b9525cdc25a447701b521d3d53131f631b535
Zach Peterson <zachqpeterson@gmail.com>
2025-12-21 16:26:26 -0700

    Add project files.

---
+++ src/Main.cpp
@@ -0,0 +1,28 @@
+#include "Defines.hpp"
+
+#define RTMIDI_DO_NOT_ENSURE_UNIQUE_PORTNAMES
+#define RTMIDI_DO_NOT_ENABLE_WORKAROUND_UWP_WRONG_TIMESTAMPS
+#include "RtMidi.h"
+
+void callback(double deltatime, std::vector<unsigned char>* message, void* userData)
+{
+    U32 nBytes = message->size();
+    for (U32 i = 0; i < nBytes; i++) { std::cout << "Byte " << i << " = " << (int)message->at(i) << ", "; }
+    if (nBytes > 0) { std::cout << "stamp = " << deltatime << std::endl; }
+}
+
+int main()
+{
+	RtMidiIn* midi = new RtMidiIn(RtMidi::UNSPECIFIED, "example");
+
+    midi->setCallback(callback, nullptr);
+    midi->ignoreTypes(false, false, false);
+
+    std::cout << "\nReading MIDI input ... press <enter> to quit.\n";
+    char input;
+    std::cin.get(input);
+
+    delete midi;
+
+    return 0;
+}
