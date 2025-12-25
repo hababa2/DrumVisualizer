fcd560a5e3c48ed3ced04b33536dfde130eff72e
Zach Peterson <zachqpeterson@gmail.com>
2025-12-25 13:36:17 -0700

    Hide Console For Release

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,13 @@
+#include "Defines.hpp"
+
 #include "Visualizer.hpp"
 
+#ifdef DV_RELEASE
+#include "Windows.h"
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+#else
 int main()
+#endif
 {
 	if (!Visualizer::Initialize()) { return -1; }
 
